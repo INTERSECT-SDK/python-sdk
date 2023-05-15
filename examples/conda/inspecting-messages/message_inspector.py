9da9f8134b0e884e87e032033d453bf6bd3cbec7
Gavin Wiggins <628-g72@users.noreply.code.ornl.gov>
2023-05-15 15:00:27 -0400

    Update inspecting messages example

--- examples/conda/inspecting-messages/message_inspector.py
+++ examples/conda/inspecting-messages/message_inspector.py
@@ -1,20 +1,14 @@
-import os
-import time
+from time import sleep
+from sys import exit, stderr
 
 from intersect import messages
 from intersect import common
 
 
 class MessageInspectorAdapter(common.Adapter):
-    def __init__(self,
-                 system_name: str,
-                 broker_address: str,
-                 broker_port: int,
-                 username: str,
-                 password: str):
-
+    def __init__(self, config: common.IntersectConfig):
         # Setup base class
-        super().__init__(system_name, broker_address, broker_port, username, password)
+        super().__init__(config)
 
         # Set up channel that listens to other status messages via wildcard topic
         self.connection.broker_client._connection.message_callback_add(
@@ -26,7 +20,7 @@ class MessageInspectorAdapter(common.Adapter):
         self.connection.broker_client._connection.loop_start()
 
         # Generate and publish start message
-        self.send(self.generate_start_status())
+        self.send(self.generate_status_starting())
 
         # Start status ticker and start action subscribe
         self.start_status_ticker()
@@ -39,27 +33,43 @@ class MessageInspectorAdapter(common.Adapter):
 
 
 if __name__ == "__main__":
-    # -- Arguments and logging --
-    system_name = os.environ.get("SYSTEM_NAME", "message-inspector")
-    parser = common.setup_parser(system_name=system_name)
-    args = parser.parse_args()
-    common.setup_logging(args)
+    # -- Config --
+    config_dict = {
+        "broker": {
+            "username": "intersect_username",
+            "password": "intersect_password",
+            "host": "127.0.0.1",
+            "port": 1883,
+        },
+        "hierarchy": {
+            "organization": "Oak Ridge National Laboratory",
+            "facility": "Inspecting Messages Facility",
+            "system": "Inspector",
+            "subsystem": "Inspector",
+            "service": "Inspector",
+        },
+    }
+
+    try:
+        config = common.load_config_from_dict(config_dict)
+    except common.IntersectConfigParseException() as ex:
+        print(ex.message, file=stderr)
+        exit(ex.returnCode)
 
     # -- Demo --
-    adapter = MessageInspectorAdapter(
-        args.system_name,
-        args.broker_address,
-        args.broker_port,
-        args.broker_username,
-        args.broker_password
-    )
+    adapter = MessageInspectorAdapter(config)
+
+    while not adapter.connection.broker_client.is_connected():
+        print("Waiting to connect to broker...", flush=True)
+        sleep(1.0)
 
     # Run until the process is killed externally
     print("Press Ctrl-C to exit:")
     try:
+        adapter.start_status_ticker()
         while True:
             # Print the uptime every second.
-            time.sleep(float(os.environ.get("UPTIME_INTERVAL", 5.0)))
-            print(f"{system_name} uptime: {int(adapter.uptime)} seconds", flush=True)
+            sleep(5.0)
+            print(f"Inspector Uptime: {int(adapter.uptime)} seconds", flush=True)
     except KeyboardInterrupt:
         print("User requested exit")
--- examples/conda/inspecting-messages/status_publisher.py
+++ examples/conda/inspecting-messages/status_publisher.py
@@ -1,49 +1,56 @@
-import os
-import time
+from time import sleep
+from sys import exit, stderr
 
 from intersect import common
 
 
 class StatusPublisherAdapter(common.Adapter):
-    def __init__(self,
-                 system_name: str,
-                 broker_address: str,
-                 broker_port: int,
-                 username: str,
-                 password: str):
-
+    def __init__(self, config: common.IntersectConfig):
         # Setup base class
-        super().__init__(system_name, broker_address, broker_port, username, password)
+        super().__init__(config)
 
         # Generate and publish start message
-        self.send(self.generate_start_status())
+        self.send(self.generate_status_starting())
 
         # Start status ticker and start action subscribe
         self.start_status_ticker()
 
 
 if __name__ == "__main__":
-    # -- Arguments and logging --
-    system_name = os.environ.get("SYSTEM_NAME", "status-publisher")
-    parser = common.setup_parser(system_name=system_name)
-    args = parser.parse_args()
-    common.setup_logging(args)
+    # -- Config --
+    config_dict = {
+        "broker": {
+            "username": "intersect_username",
+            "password": "intersect_password",
+            "host": "127.0.0.1",
+            "port": 1883,
+        },
+        "hierarchy": {
+            "organization": "Oak Ridge National Laboratory",
+            "facility": "Inspecting Messages Facility",
+            "system": "Publisher",
+            "subsystem": "Publisher",
+            "service": "Publisher",
+        },
+    }
+
+    try:
+        config = common.load_config_from_dict(config_dict)
+    except common.IntersectConfigParseException() as ex:
+        print(ex.message, file=stderr)
+        exit(ex.returnCode)
 
     # -- Demo --
-    adapter = StatusPublisherAdapter(
-        args.system_name,
-        args.broker_address,
-        args.broker_port,
-        args.broker_username,
-        args.broker_password
-    )
+    adapter = StatusPublisherAdapter(config)
 
     # Run until the process is killed externally
     print("Press Ctrl-C to exit:")
     try:
+        adapter.start_status_ticker()
         while True:
             # Print the uptime every second.
-            time.sleep(float(os.environ.get("UPTIME_INTERVAL", 5.0)))
-            print(f"{system_name} uptime: {int(adapter.uptime)} seconds", flush=True)
+            sleep(5.0)
+            print(f"Publisher Uptime: {int(adapter.uptime)} seconds", flush=True)
     except KeyboardInterrupt:
         print("User requested exit")
+
