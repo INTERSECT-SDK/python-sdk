48a213188968ad72e7a37bfd3999cd18c936003e
Lance Drane <dranelt@ornl.gov>
2023-06-14 18:10:14 -0400

    proper conda generation script, add version_info attribute
    Signed-off-by: Lance Drane <dranelt@ornl.gov>

--- src/intersect/__init__.py
+++ src/intersect/__init__.py
@@ -1,3 +1,3 @@
 # flake8: noqa
 
-from .version import __version__
+from .version import __version__, version_info
--- src/intersect/version.py
+++ src/intersect/version.py
@@ -1,1 +1,2 @@
 __version__ = "0.1.2"
+version_info = tuple([int(x) for x in __version__.split(".")])
