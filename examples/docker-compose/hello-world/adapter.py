73cfb97e8f0f00c3f85fb91b4cd63bff37e1b92a
Marshall McDonnell <mcdonnellmt@ornl.gov>
2023-08-04 16:30:16 -0400

    Re #44 - fix to docker compose to align with other examples

--- examples/docker-compose/hello-world/adapter.py
+++ examples/docker-compose/hello-world/adapter.py
@@ -5,7 +5,7 @@ from sys import exit, stderr
 from time import sleep
 
 # intersect imports
-from intersect_sdk import adapter, config_functions, exceptions, messages, IntersectConfig
+from intersect_sdk import IntersectConfig, adapter, config_functions, exceptions, messages
 
 
 class HelloWorldAdapter(adapter.Adapter):
--- examples/docker-compose/hello-world/requestor.py
+++ examples/docker-compose/hello-world/requestor.py
@@ -6,7 +6,7 @@ from sys import exit, stderr
 from time import sleep
 
 # intersect imports
-from intersect_sdk import adapter, config_functions, exceptions, messages, IntersectConfig
+from intersect_sdk import IntersectConfig, adapter, config_functions, exceptions, messages
 
 
 class HelloWorldRequestor(adapter.Adapter):
