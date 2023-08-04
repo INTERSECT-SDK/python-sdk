265a02eb1572d44e4c822818bb5561d853707f83
Marshall McDonnell <mcdonnellmt@ornl.gov>
2023-08-04 16:30:16 -0400

    Re #44 - fix to docker compose to align with other examples

--- examples/docker-compose/hello-world/requestor.py
+++ examples/docker-compose/hello-world/requestor.py
@@ -6,7 +6,7 @@ from sys import exit, stderr
 from time import sleep
 
 # intersect imports
-from intersect_sdk import adapter, config_functions, exceptions, messages, IntersectConfig
+from intersect_sdk import IntersectConfig, adapter, config_functions, exceptions, messages
 
 
 class HelloWorldRequestor(adapter.Adapter):
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--destination",
         type=str,
-        default=environ.get("DESTINATION", "hello-world-adapter"),
+        default=environ.get("DESTINATION", "Adapter"),
         help="System name of the destination for the request",
     )
     args = parser.parse_args()
