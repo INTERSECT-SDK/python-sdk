875db1890856bdb23ea41c8e936bc0e216267780
Lance Drane <dranelt@ornl.gov>
2024-02-15 14:46:08 -0500

    bump to 0.5.1
    Signed-off-by: Lance Drane <dranelt@ornl.gov>

--- src/intersect_sdk/version.py
+++ src/intersect_sdk/version.py
@@ -4,7 +4,7 @@ These values are often used programmatically by the SDK, but can be used by appl
 """
 from __future__ import annotations
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 """
 Version string in the format <MAJOR>.<MINOR>.<DEBUG> . Follows semantic versioning rules.
 """
