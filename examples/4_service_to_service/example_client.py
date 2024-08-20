16e719bd8a646fbee67e6ea6ca4347ee153ec1e5
Gregory Cage <cagege@ornl.gov>
2024-08-20 15:25:23 -0400

    Add initial example for service to service communication

---
+++ examples/4_service_to_service/example_client.py
@@ -0,0 +1,78 @@
+"""Client for service to service example.
+
+Kicks off exmaple by sending message to service one, and then
+waits for an event from service one to confirm the messages were passed between the two services properly.
+
+"""
+
+import logging
+
+from intersect_sdk import (
+    INTERSECT_JSON_VALUE,
+    IntersectClient,
+    IntersectClientCallback,
+    IntersectClientConfig,
+    IntersectDirectMessageParams,
+    default_intersect_lifecycle_loop,
+)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class SampleOrchestrator:
+    """Simply contains an event callback for events from Service One."""
+
+    def event_callback(
+        self, _source: str, _operation: str, _event_name: str, payload: INTERSECT_JSON_VALUE
+    ) -> None:
+        """This simply prints the event from Service One  to your console.
+
+        Params:
+          source: the source of the response message.
+          operation: the name of the function we called in the original message.
+          _has_error: Boolean value which represents an error.
+          payload: Value of the response from the Service.
+        """
+        logger.info('making it to event callback')
+        print(payload)
+
+
+if __name__ == '__main__':
+    from_config_file = {
+        'brokers': [
+            {
+                'username': 'intersect_username',
+                'password': 'intersect_password',
+                'port': 1883,
+                'protocol': 'mqtt3.1.1',
+            },
+        ],
+    }
+
+    # The counter will start after the initial message.
+    # If the service is already active and counting, this may do nothing.
+    initial_messages = [
+        IntersectDirectMessageParams(
+            destination='example-organization.example-facility.example-system.example-subsystem.service-one',
+            operation='ServiceOne.pass_text_to_service_2',
+            payload='Kicking off the example!',
+        )
+    ]
+    config = IntersectClientConfig(
+        initial_message_event_config=IntersectClientCallback(
+            messages_to_send=initial_messages,
+            services_to_start_listening_for_events=[
+                'example-organization.example-facility.example-system.example-subsystem.service-one'
+            ],
+        ),
+        **from_config_file,
+    )
+    orchestrator = SampleOrchestrator()
+    client = IntersectClient(
+        config=config,
+        event_callback=orchestrator.event_callback,
+    )
+    default_intersect_lifecycle_loop(
+        client,
+    )
