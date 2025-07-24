b645ae2e2989a4fe817c99588ef3ac52d89198c1
Lance-Drane <Lance-Drane@users.noreply.github.com>
2025-07-24 18:11:12 -0400

    fix docstring
    Signed-off-by: Lance-Drane <Lance-Drane@users.noreply.github.com>

--- src/intersect_sdk/core_definitions.py
+++ src/intersect_sdk/core_definitions.py
@@ -30,10 +30,16 @@ These values are used to help map an output (or a part of an output) from an arb
 
 In general, mime types follow one of two rules:
 - Complex types (types which cannot be represented as a sequence of bytes) MUST be represented by a Content-Type of 'application/json' (this is default).
+
   If a complex type has binary data in a field, this field MUST be Base64 encoded.
+
   You can mark the type with either 'pydantic.Base64Bytes', or if you need the value to be URL safe, 'pydantic.Base64UrlBytes'. You MUST also specify the "contentType" property, like this:
+
+    ```
     field: Annotated[pydantic.Base64Bytes, pydantic.Field(json_schema_extra={"contentType": "image/png"})]
+    ```
 
   INTERSECT is able to handle serialization/deserialization of 'application/json' types for you, though note that you will need to verify binary data (incoming and outgoing) yourself. INTERSECT will handle the Base64 encoding/decoding, though.
+
 - If your Content-Type value is ANYTHING ELSE, you MUST mark it as "bytes" . In this instance, INTERSECT will not base64-encode or base64-decode the value.
 """
